723c50259852ac195b38ea3caeacc7bddfeb1f80
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-05-22 14:28:56 +0200

    Avec Css

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion des Tâches</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<!-- Contenu de votre page -->
+</body>
+</html>
+<!------------------------------------------------------------------------------------------------------------->
     <div class="container">
         <h1>Create Task</h1>
         <form action="{{ route('tasks.store') }}" method="POST">
