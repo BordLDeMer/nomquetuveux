6e4178e42ca9588278d0e3c304f6e6dd06a254ff
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-05-22 14:00:06 +0200

    Kdo d'imposteur pour Olivier

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,17 @@
+    <div class="container">
+        <h1>Create Task</h1>
+        <form action="{{ route('tasks.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input value="{{$task->title}}" type="text" name="title" id="title" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" class="form-control" required>
+                    {{$task->description}}
+                </textarea>
+            </div>
+            <button type="submit" class="btn btn-success">Save</button>
+        </form>
+    </div>
