d5377739633c9d913d53187ae93d8f7f6aa907a3
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-06-23 16:39:09 +0200

    Buggué

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion des Tâches</title>
-
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-<!-- Contenu de votre page -->
-</body>
-</html>
-<!------------------------------------------------------------------------------------------------------------->
+@extends('layouts.app')
+@section('content')
 
 <div class="container">
         <h1>Task Manager</h1>
@@ -49,3 +37,4 @@
             <p>No tasks available.</p>
         @endif
     </div>
+@endsection
